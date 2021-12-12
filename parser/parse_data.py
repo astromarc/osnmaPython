165fa109f3c8cd8bb45207357ae9c2b33a813bf8
Joan Ametller Esquerra <jametller@indra.es>
2021-12-12 22:01:09 +0100

    taking into account bits are reversed in words

--- parser/parse_data.py
+++ parser/parse_data.py
@@ -11,10 +11,9 @@ with open('../data_processed.csv') as csvfile:
             continue
         word5=int(row[12])
         word6=int(row[13])
- #       print ("SVID:" + row[1] + " word5: " + hex(word5) + " word6: " + hex(word6))
- #       print ("HEX SVID:" + row[1] + " word5: " + hex((word5 & 0x00003FFF) << 26) + " word6: " + hex((word6 & 0xFFFFFFC0) >> 6) + " Combined words: " +  hex((word5 & 0x00003FFF) << 26 | (word6 & 0xFFFFFFC0) >> 6))
- #       print ("BINARY SVID:" + row[1] + " word5: " + bin((word5 & 0x00003FFF) << 26) + " word6: " + bin((word6 & 0xFFFFFFC0) >> 6) + " Combined words: " +  bin((word5 & 0x00003FFF) << 26 | (word6 & 0xFFFFFFC0) >> 6))
         if row[1] == "27":
-            print ("SVID: " + row[1] + " HKROOT BYTE: " + hex((word5 & 0x00003FFF) >> 6))
-            hkroot_sequence += hex((word5 & 0x00003FFF) >> 6) + ","
+            hkroot_byte = (word6 & 0x00003FC0) >> 6  # Take first byte (Starting from the end)
+            hkroot_byte_r = int('{:08b}'.format(hkroot_byte)[::-1], 2) # Reverse bits
+            print ("SVID: " + row[1] + " HKROOT BYTE: " + hex(hkroot_byte) + " HKROOT BYTE (REVERSED): " + hex(hkroot_byte_r))
+            hkroot_sequence += hex(hkroot_byte_r) + ","
 print("HKROOT Stream for SVID 27: " + hkroot_sequence)
