db16c46241b1be0b811d2af00dd0826f5ee6b112
Joan Ametller Esquerra <jametller@indra.es>
2021-12-31 17:00:46 +0100

    parsing DSM-kroot with lookup tables

--- parser/parse_data.py
+++ parser/parse_data.py
@@ -18,22 +18,32 @@ def convert_mack_words_to_bytearray(words):
         last |= i
     return last
 
+Nbdk_lookup = [None, 728, 832, 936, 1040, 1144, 1248, 1352, 1456, None, None, None, None, None, None, None]
+HF_lookup = ["SHA-256", None, "SHA3-256", None]
+MF_lookup = ["HMAC-SHA-256", "CMAC-AES", None, None]
+KS_lookup = [96, 104, 112, 120, 128, 160, 192, 224, 256, None, None, None, None, None, None, None]
+TS_lookup = [None, None, None, None, None, 20, 24, 28, 32, 40, None, None, None, None, None, None]
+
 def parse_dsm_kroot_msg (msg):
     parsed_dsm_kroot = {}
-    stream = msg.getDSMStream()
-    parsed_dsm_kroot["NB_dk"] = (stream[0] & 0xF0) >> 4
+    stream = msg.getDSMBytes()
+    parsed_dsm_kroot["NBdk"] = Nbdk_lookup[(stream[0] & 0xF0) >> 4]
     parsed_dsm_kroot["PKID"] = stream[0] & 0x0F
     parsed_dsm_kroot["CIDKR"] = (stream[1] & 0xC0) >> 6
     parsed_dsm_kroot["Reserved1"] = (stream[1] & 0x30) >> 4
-    parsed_dsm_kroot["HF"] = (stream[1] & 0x0C) >> 2
-    parsed_dsm_kroot["MF"] = stream[1] & 0x03
-    parsed_dsm_kroot["KS"] = (stream[2] & 0xF0) >> 4
-    parsed_dsm_kroot["TS"] = stream[2] & 0x0F
+    parsed_dsm_kroot["HF"] = HF_lookup[(stream[1] & 0x0C) >> 2]
+    parsed_dsm_kroot["MF"] = MF_lookup[stream[1] & 0x03]
+    parsed_dsm_kroot["KS"] = KS_lookup[(stream[2] & 0xF0) >> 4]
+    parsed_dsm_kroot["TS"] = TS_lookup[stream[2] & 0x0F]
     parsed_dsm_kroot["MACLT"] = stream[3]
     parsed_dsm_kroot["Reserved2"] = (stream[4] & 0xF0) >> 4
     parsed_dsm_kroot["WNk"] = ((stream[4] & 0x0F) << 8) |stream[5]
     parsed_dsm_kroot["TOWHk"] = stream[6]
     parsed_dsm_kroot["alfa"] = (stream[7] << 40) | (stream[8] << 32) | (stream[9] << 24) | (stream[10] << 16) | (stream[11] << 8) | stream[12]
+    bytes_key = parsed_dsm_kroot["KS"] // 8
+    parsed_dsm_kroot["KROOT"] = stream[13:13+bytes_key]
+    parsed_dsm_kroot["DS+Padding"] = stream[13+bytes_key:]
+    return parsed_dsm_kroot
 
 
 def parse_dsm_pkr_msg (msg):
@@ -154,5 +164,5 @@ with open('../data_mataro2.csv') as csvfile:
             hkroot_sequence += hex(hkroot_byte) + ","
         else:
             logging.debug("0 OSNMA WORD for SVID: " + row[1])
-logging.debug("HKROOT Stream for SVID 27: " + hkroot_sequence)
 logging.info (str(dsm_messages))
+logging.info(str(parse_dsm_kroot_msg(dsm_messages[4])))
