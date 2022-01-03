f65cb8491a8a34b76887b3644bbe559e48d2fd07
astromarc <marc.c.fargas@gmail.com>
2022-01-03 17:23:11 +0100

    Removed adquisition of repeated sentences
    - Remove Adquinision of Repeated Sentences
    - Added chronometer since last adquisition
    - Changed Serial parameters to match pygpsclient

--- ublox/getSerialData.py
+++ ublox/getSerialData.py
@@ -3,31 +3,47 @@
 from serial import Serial
 from pyubx2 import UBXReader
 import csv
-  
-
-def remove_prefix(text, prefix):
-    return text[text.startswith(prefix) and len(prefix):]
+from datetime import datetime
 
 fields = ['gnssId', 'svId', 'reserved0', 'freqId', 'numWords', 'chn', 'version', 'reserved1', 'dwrd_01', 'dwrd_02', 'dwrd_03'
-, 'dwrd_04', 'dwrd_05', 'dwrd_06', 'dwrd_07',  'dwrd_08', 'dwrd_09'] 
+, 'dwrd_04', 'dwrd_05', 'dwrd_06', 'dwrd_07',  'dwrd_08', 'dwrd_09']
+
+ubxList_old = []
+ubxList = []
+now_receiver = datetime.now()
+time_receiver = now_receiver.strftime("%H:%M:%S.%f'")[:-3]
 
 while True:
-   stream = Serial('COM19', 9600, timeout=1)
+   record = False
+   ubxList_new = []
+   ubxList = []
+   now = datetime.now()
+   current_time = now.strftime("%H:%M:%S.%f'")[:-3]
+   stream = Serial('COM6', 9600, bytesize=8, parity='N', stopbits=1,)
    ubr = UBXReader(stream)
    (raw_data, parsed_data) = ubr.read()
    if parsed_data is not None:
-      ubxList = str(parsed_data).split(",")
+      ubxList_new = str(parsed_data).split(",")
    stream.close()
-   list = []
-
-   for x in ubxList:
-      if x != "<UBX(RXM-SFRBX":
-         ubxSplit = x.split('=')[1].split(")")[0]
-         #print(ubxSplit)
-         list.append(ubxSplit)
-
-   with open('data.csv', 'a', newline='') as f:
-    write = csv.writer(f)
-    write.writerow(list)
-    f.close()
-   print(list)
+   
+   if ubxList_new == ubxList_old: #We have received the same info from uBlox receiver
+      record = False
+   if (ubxList_new != ubxList_old) and (ubxList_new != []): #New info from uBlox Receier
+      record = True
+      ubxList_old = ubxList_new
+      now_receiver = now
+      
+   deltaTime = (now - now_receiver).total_seconds()
+   
+   print(current_time,"Seconds since last reception:",deltaTime,"Record Status:",record,"Received:",ubxList_new)
+   
+   if record:
+      ubxList.append(current_time)
+      for x in ubxList_new:
+         if x != "<UBX(RXM-SFRBX":
+            ubxSplit = x.split('=')[1].split(")")[0]
+            ubxList.append(ubxSplit)
+      with open('data4.csv', 'a', newline='') as f:
+         write = csv.writer(f)
+         write.writerow(ubxList)
+         f.close()
