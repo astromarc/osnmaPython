e2227278012aafcf259d3726af0c1294a018140d
Joan Ametller Esquerra <jametller@indra.es>
2022-03-06 15:19:04 +0100

    Interface based on objects containing method, rather than plain method

--- parser/oop/test_ublox.py
+++ parser/oop/test_ublox.py
@@ -5,13 +5,12 @@ rx_page = '18:14:13.7413,Galileo,3,1,0,9,4,2,0,86966039,4045406688,2282300530,29
 rx_page_data_only_1 = '18:14:13.7413,Galileo,3,1,0,9,4,2,0,714440703,4294967295,4294967295,4294950912,359301120,0,0,0,1'
 
 class TestUbloxParser(unittest.TestCase):
+    def read_page(self):
+        return rx_page_data_only_1
+    def process_page (self, svid, page_type, osnma, inav_data):
+        self.assertEqual(inav_data, bytearray([0xaa, 0x55, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x55, 0xaa]))
     def test_create(self):
-        def test_page_reader():
-            return rx_page_data_only_1
-        def test_page_processor (svid, page_type, osnma, inav_data):
-            self.assertEqual(inav_data, bytearray([0xaa, 0x55, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x55, 0xaa]))
-
-        ubrx = ublox.UbloxParser(test_page_reader, test_page_processor)
+        ubrx = ublox.UbloxParser(self, self)
         self.assertNotEqual(ubrx, None)
         ubrx.process_page()
 
--- parser/oop/ublox.py
+++ parser/oop/ublox.py
@@ -1,10 +1,12 @@
 
 class UbloxParser:
+    # page_reader Arbitrary object containing the implementation of read_page() method from where pages are physically read (e.g. serial line)
+    # page_processor Arbitrary object containing the implementation of process page method where pages are passed (e.g. Galileo Page Parser)
     def __init__(self, page_reader, page_processor):
         self.__page_reader = page_reader
         self.__page_processor = page_processor
     def process_page(self):
-        inav = self.__page_reader()
+        inav = self.__page_reader.read_page()
         inav = inav.split(',')
         svid = int(inav[2])
         word1 = int(inav[9])
@@ -22,4 +24,4 @@ class UbloxParser:
         word_type = (word1 & 0x3F000000) >> 24
         data_word = ((0x3FFFFFFF & word1) <<  98) | (word2 << 66) | (word3 << 34) | (((word4 & 0xFFFFC000) >> 14) << 16) | ((word5 & 0x3FFFC000) >> 14)
 
-        self.__page_processor(svid, word_type, osnma, data_word.to_bytes(16, 'big'))
+        self.__page_processor.process_page(svid, word_type, osnma, data_word.to_bytes(16, 'big'))
